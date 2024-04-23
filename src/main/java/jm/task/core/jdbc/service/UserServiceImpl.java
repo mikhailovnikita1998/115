f8b131f1e7095fb5a238ecc5b359dc7c636ed350
Никита <148373103+mikhailovnikita1998@users.noreply.github.com>
2024-04-23 17:31:43 +0900

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.Users2;
 
 import java.sql.SQLException;
